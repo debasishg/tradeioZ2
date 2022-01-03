1b96d05a87489a235b8175eea53793a2d6c5dee7
Debasish Ghosh <ghosh.debasish@gmail.com>
2022-01-03 22:06:41 +0530

    Update main program (refactoring)

--- modules/core/src/main/scala/tradex/domain/Main.scala
+++ modules/core/src/main/scala/tradex/domain/Main.scala
@@ -3,24 +3,27 @@ package tradex.domain
 import zio._
 import zio.prelude.NonEmptyList
 
-import services.trading.{ TradingService, TradingServiceError }
-import TradingService._
-import TradingServiceError._
+import services.trading.TradingService
 import services.accounting.AccountingService
 import java.time._
 import model.trade._
 import model.user._
 import model.balance._
 
-object Main extends zio.ZIOApp {
+object Main extends ZIOApp {
   override type Environment = TradingService with AccountingService
 
   override implicit def tag: Tag[Environment] = Tag[Environment]
 
   override def layer: ZLayer[ZIOAppArgs, Any, Environment] = Application.prod.appLayer
 
-  override def run: ZIO[TradingService, TradingError, Unit] = {
-    ZIO.serviceWithZIO[TradingService](
+  override def run = {
+    val setupDB: ZIO[config.Config, Throwable, Unit] = for {
+      dbConf <- config.getDbConfig
+      _      <- FlywayMigration.migrate(dbConf)
+    } yield ()
+
+    setupDB *> ZIO.serviceWithZIO[TradingService](
       _.getAccountsOpenedOn(LocalDate.EPOCH).map(_.foreach(println))
     )
   }
