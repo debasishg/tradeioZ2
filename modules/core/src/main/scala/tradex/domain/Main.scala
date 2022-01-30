722a47ffd2f189c641468be9a6a846220805fb90
Debasish Ghosh <ghosh.debasish@gmail.com>
2022-01-30 20:43:01 +0530

    Fixed compilation error after zio version upgrade

--- modules/core/src/main/scala/tradex/domain/Main.scala
+++ modules/core/src/main/scala/tradex/domain/Main.scala
@@ -11,19 +11,19 @@ import model.user._
 import model.balance._
 
 object Main extends ZIOApp {
-  override type Environment = config.Config with TradingService with AccountingService
+  override type Environment = config.Config // with TradingService with AccountingService
 
   override implicit def tag: Tag[Environment] = Tag[Environment]
 
   override def layer: ZLayer[ZIOAppArgs, Any, Environment] = Application.prod.appLayer
 
-  override def run: ZIO[config.Config with TradingService, Throwable, Unit] = {
+  override def run = {
     val setupDB: ZIO[config.Config, Throwable, Unit] = for {
       dbConf <- config.getDbConfig
       _      <- FlywayMigration.migrate(dbConf)
     } yield ()
 
-    ZIO.serviceWithZIO[config.Config with TradingService](service =>
+    ZIO.serviceWithZIO[TradingService](service =>
       setupDB *> service.getAccountsOpenedOn(LocalDate.EPOCH).map(_.foreach(println))
     )
   }
@@ -34,13 +34,11 @@ object Program {
       frontOfficeInput: GenerateTradeFrontOfficeInput,
       userId: UserId
   ): ZIO[TradingService with AccountingService, Throwable, (NonEmptyList[Trade], NonEmptyList[Balance])] = {
-
-    ZIO
-      .serviceWithZIO[TradingService with AccountingService](service =>
-        for {
-          trades   <- service.generateTrade(frontOfficeInput, userId)
-          balances <- service.postBalance(trades)
-        } yield (trades, balances)
-      )
+    for {
+      tr       <- ZIO.service[TradingService]
+      ac       <- ZIO.service[AccountingService]
+      trades   <- tr.generateTrade(frontOfficeInput, userId)
+      balances <- ac.postBalance(trades)
+    } yield (trades, balances)
   }
 }
