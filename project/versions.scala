11385d5791c6c8c77225d1ff867822eefb10039c
Scala Steward <scala_steward@virtuslab.com>
2022-08-04 13:17:13 +0000

    Update zio-config, zio-config-magnolia, ... to 3.0.2

--- project/versions.scala
+++ project/versions.scala
@@ -31,7 +31,7 @@ object Versions {
   val semanticDBVersion       = "4.4.35"
   val zioVersion              = "2.0.0"
   val zioPreludeVersion       = "1.0.0-RC15"
-  val zioConfigVersion        = "3.0.1"
+  val zioConfigVersion        = "3.0.2"
   val zioInteropCatsVersion   = "3.3.0"
   val enumeratumDoobieVersion = "1.7.1"
   val flywayDbVersion         = "8.4.4"
