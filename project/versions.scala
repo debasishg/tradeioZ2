0225369b0ef3aeaf3b4a9a5cabc18a5afdc119b6
Scala Steward <scala_steward@virtuslab.com>
2024-01-20 14:38:38 +0000

    Update pureconfig to 0.17.5

--- project/versions.scala
+++ project/versions.scala
@@ -7,7 +7,7 @@ object Versions {
   val scalaVersion            = "2.13.8"
   val silencerVersion         = "1.4.4"
   val log4j2Version           = "2.13.1"
-  val pureconfigVersion       = "0.17.1"
+  val pureconfigVersion       = "0.17.5"
   val doobieVersion           = "1.0.0-RC2"
   val newtypeVersion          = "0.4.4"
   val refinedVersion          = "0.10.1"
